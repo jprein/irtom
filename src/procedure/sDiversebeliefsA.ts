c74ce9b7ff978e71f96462ae0b213e48b7e4c10b
Mayur Odedra <74038945+MayurOdedra40@users.noreply.github.com>
2025-05-27 18:23:28 +0200

    added sDiverseBelief A and B (#48)

---
+++ src/procedure/sDiversebeliefsA.ts
@@ -0,0 +1,34 @@
+import type { SvgInHtml } from '../types';
+import { playPromise } from '../util/audio';
+import { sleep } from '../util/helpers';
+import { hideTwoOptions } from '../util/hideTwoOptions';
+import { showTwoOptions } from '../util/showTwoOptions';
+import { swapSlides } from '../util/slideVisibility';
+import { gsap } from 'gsap';
+
+export default async ({ currentSlide, previousSlide }) => {
+	// Name of slide
+	const slidePrefix = 's-diversebeliefs-a';
+
+	// Store correct response
+	data.procedure[data.currentSlide].correct = 'left';
+
+	// Swap slides
+	swapSlides(currentSlide, previousSlide);
+	data.simpleSlideCounter++;
+
+	// In beginning, hide response options
+	await hideTwoOptions(slidePrefix);
+
+	// Short break before showing response options
+	await sleep(1000);
+
+	const boy = document.getElementById(`${slidePrefix}-boy`) as SvgInHtml;
+	await data.sprite.playPromise(`${slidePrefix}-1`);
+	await gsap.timeline().to(boy, {
+		autoAlpha: 0,
+		duration: 1,
+	});
+
+	await showTwoOptions(slidePrefix);
+};
