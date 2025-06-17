3c7f66ef17d54c8fa087b89f1c18071783417e9e
Julia Prein <26962518+jprein@users.noreply.github.com>
2025-06-17 10:54:59 +0200

    Closes #71. Add new false belief change of location trial

---
+++ src/procedure/sFalsebeliefColA.ts
@@ -0,0 +1,150 @@
+import { gsap } from 'gsap';
+import type { SvgInHtml } from '../types';
+import { swapSlides } from '../util/slideVisibility';
+import { sleep } from '../util/helpers';
+import { hideTwoOptions } from '../util/hideTwoOptions';
+import { showTwoOptions } from '../util/showTwoOptions';
+
+export default async ({ currentSlide, previousSlide }) => {
+	// Name of slide
+	const slidePrefix = 's-falsebelief-col-a';
+
+	// Store correct response
+	data.procedure[data.currentSlide].correct = 'right';
+
+	// Swap slides
+	swapSlides(currentSlide, previousSlide);
+	data.simpleSlideCounter++;
+
+	// Trial-specific animation
+	// Get all relevant elements
+	const girl = document.getElementById(
+		`link-${slidePrefix}-${data.community}-girl`,
+	) as SvgInHtml;
+	const girlKneeling = document.getElementById(
+		`link-${slidePrefix}-${data.community}-girl-kneeling`,
+	) as SvgInHtml;
+	const dogRunning = document.getElementById(
+		`link-${slidePrefix}-dog-running`,
+	) as SvgInHtml;
+	const dogLying = document.getElementById(
+		`link-${slidePrefix}-dog-lying`,
+	) as SvgInHtml;
+
+	// Define animation function
+	async function showAnimation() {
+		// Initially hide some agent elements
+		gsap.set([girlKneeling, dogLying], {
+			autoAlpha: 0,
+			x: 0,
+		});
+		gsap.set(girl, {
+			autoAlpha: 1,
+			x: 0,
+		});
+		gsap.set(dogRunning, { autoAlpha: 1, x: 1200 });
+
+		// Play initial audio
+		await data.sprite.playPromise(`${slidePrefix}-1`);
+
+		// Animation sequence
+		await gsap
+			.timeline()
+			.to(dogRunning, {
+				x: 0,
+				duration: 3,
+				onComplete: () => {
+					data.sprite.play(`${slidePrefix}-2`);
+				},
+			})
+			.to(dogRunning, {
+				delay: data.spriteJSON.sprite[`${slidePrefix}-2`][1] / 1000 + 1,
+				autoAlpha: 0,
+				duration: 0.1,
+			})
+			.to(
+				dogLying,
+				{
+					autoAlpha: 1,
+					duration: 0.1,
+					onComplete: () => {
+						data.sprite.play(`${slidePrefix}-3`);
+					},
+				},
+				'<',
+			)
+			.to(girl, {
+				delay: data.spriteJSON.sprite[`${slidePrefix}-3`][1] / 1000,
+				autoAlpha: 0,
+				duration: 0.1,
+			})
+			.to(
+				girlKneeling,
+				{
+					autoAlpha: 1,
+					duration: 0.1,
+					onComplete: () => {
+						data.sprite.play(`${slidePrefix}-4`);
+					},
+				},
+				'<',
+			)
+			.to(dogLying, {
+				delay: data.spriteJSON.sprite[`${slidePrefix}-4`][1] / 1000 - 2,
+				autoAlpha: 0,
+				duration: 0.5,
+			})
+			.to(girlKneeling, { delay: 1, autoAlpha: 0, duration: 0.1 })
+			.to(girl, { autoAlpha: 1, duration: 0.1 }, '<')
+			.to(girl, {
+				delay: 1,
+				x: -1200,
+				duration: 3,
+				onComplete: () => {
+					data.sprite.play(`${slidePrefix}-5`);
+				},
+			})
+			.to(dogLying, {
+				delay: data.spriteJSON.sprite[`${slidePrefix}-5`][1] / 1000 - 2,
+				autoAlpha: 1,
+				duration: 0.1,
+			})
+			.to(dogLying, {
+				delay: 2,
+				autoAlpha: 0,
+				duration: 0.1,
+				onComplete: () => {
+					data.sprite.play(`${slidePrefix}-6`);
+				},
+			})
+			.to(dogRunning, { autoAlpha: 1, duration: 0.1 }, '<')
+			.to(dogRunning, {
+				delay: data.spriteJSON.sprite[`${slidePrefix}-6`][1] / 1000 - 3.5,
+				x: -650,
+				duration: 2,
+			})
+			.to(dogRunning, {
+				delay: 1.5,
+				autoAlpha: 0,
+				duration: 0.1,
+			})
+			.to(dogLying, { autoAlpha: 1, x: -630, duration: 0 }, '<')
+			.to(dogLying, {
+				delay: 1.5,
+				autoAlpha: 0,
+				duration: 0.1,
+			});
+	}
+
+	// In beginning, hide response options
+	await hideTwoOptions(slidePrefix);
+
+	// Show animation
+	await showAnimation();
+
+	// Short break before showing response options
+	await sleep(1000);
+
+	// Show left/right response options and store participant response
+	await showTwoOptions(slidePrefix);
+};
