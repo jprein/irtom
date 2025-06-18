a6e3516c77dd3f1cab43f63d45978d97bad32903
Julia Prein <26962518+jprein@users.noreply.github.com>
2025-06-18 11:16:18 +0200

    Prepare for falsebelief unexpected content a

---
+++ src/procedure/sFalsebeliefUcA.ts
@@ -0,0 +1,38 @@
+import { gsap } from 'gsap';
+import type { SvgInHtml } from '../types';
+import { swapSlides } from '../util/slideVisibility';
+import { sleep } from '../util/helpers';
+import { hideTwoOptions } from '../util/hideTwoOptions';
+import { showTwoOptions } from '../util/showTwoOptions';
+
+export default async ({ currentSlide, previousSlide }) => {
+	// Name of slide
+	const slidePrefix = 's-falsebelief-uc-a';
+
+	// Store correct response
+	data.procedure[data.currentSlide].correct = 'right';
+
+	// Swap slides
+	swapSlides(currentSlide, previousSlide);
+	data.simpleSlideCounter++;
+
+	// Trial-specific animation
+	// Get all relevant elements
+
+	// Define animation function
+	async function showAnimation() {
+		await data.sprite.playPromise(`${slidePrefix}-1`);
+	}
+
+	// In beginning, hide response options
+	await hideTwoOptions(slidePrefix);
+
+	// Show animation
+	await showAnimation();
+
+	// Short break before showing response options
+	await sleep(1000);
+
+	// Show left/right response options and store participant response
+	await showTwoOptions(slidePrefix);
+};
