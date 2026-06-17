c44aef5c0ba15f2b79401f3735c2feb3581f5e58
Julia Prein <26962518+jprein@users.noreply.github.com>
2026-06-17 08:52:54 +0200

    Update audio start button text for consistency in language

--- src/app.ts
+++ src/app.ts
@@ -193,7 +193,7 @@ const withTimeout = async <T>(
 		await new Promise<void>((resolve) => {
 			const onStart = async () => {
 				const originalLabel =
-					button.textContent ?? 'Klicke hier, um mit der Studie zu beginnen';
+					button.textContent ?? 'Hier klicken, um mit der Studie zu beginnen';
 				button.disabled = true;
 				const preparingLabel =
 					buttonTranslations.preparingAudio[
